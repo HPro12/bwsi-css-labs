e2f7a9cb2104c24d73d99d52f6534c24d9b149f7
HPro12 <haoyu.zhang2008@gmail.com>
2026-03-19 16:41:20 +0800

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,14 +1,14 @@
 """
 lab_1a.py
 
-The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
+The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 11
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
 
 def main():
     print("Hello World!")
 
-    name = "" # TODO: Insert your name between the double quotes
+    name = "Haoyu Zhang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
 
